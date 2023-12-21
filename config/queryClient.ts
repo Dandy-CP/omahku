cd7345ac3905671766165c96a8232b46fe4db7e5
Dandy Candra <dandycandrapratama@gmail.com>
2023-12-21 23:02:07 +0700

    add query config

---
+++ config/queryClient.ts
@@ -0,0 +1,49 @@
+import axios from "axios";
+import qs from "querystring";
+import { QueryClient } from "@tanstack/react-query";
+import { QueryFetchOptions, ApiError } from "@/types/client.type";
+
+export const client = axios.create({
+  baseURL: "https:/mocki.io/v1/",
+  timeout: 45000,
+  headers: {
+    Accept: "application/json",
+  },
+});
+
+export async function queryFetch({
+  url,
+  inputParams,
+}: QueryFetchOptions): Promise<any> {
+  let params = "";
+
+  if (inputParams) {
+    params = qs.stringify(inputParams);
+  }
+
+  return new Promise(async (resolve, reject) => {
+    try {
+      let fetchUrl = url;
+
+      if (params) {
+        fetchUrl += "?" + params;
+      }
+
+      const res = await client.get(fetchUrl);
+      const json = await res.data;
+
+      resolve(json);
+    } catch (e: any) {
+      reject(e as ApiError);
+    }
+  });
+}
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+});

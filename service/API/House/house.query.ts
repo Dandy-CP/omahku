8e37e709e7d5a1d6d10504747c8b6fb274c40eea
Dandy Candra <dandycandrapratama@gmail.com>
2023-12-21 23:03:15 +0700

    add function query get

---
+++ service/API/House/house.query.ts
@@ -0,0 +1,18 @@
+import { useQuery, UseQueryOptions } from "@tanstack/react-query";
+import { queryFetch } from "@/config/queryClient";
+import { ApiError } from "@/types/client.type";
+import { IResponseApiHouse } from "@/types/house.type";
+
+export function GetListHouse(
+  options?: UseQueryOptions<IResponseApiHouse[], ApiError>
+) {
+  return useQuery<IResponseApiHouse[], ApiError>({
+    queryKey: ["house"],
+    queryFn: async () => {
+      return await queryFetch({
+        url: "9d14be61-2ef1-4b2d-9246-3bf36e60687b",
+      });
+    },
+    ...options,
+  });
+}

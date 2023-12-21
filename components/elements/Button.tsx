a58543d3fd91aa557c1da5159b4077bdfd6619ea
Dandy Candra <dandycandrapratama@gmail.com>
2023-12-21 14:31:51 +0700

    add variant button white

--- components/elements/Button.tsx
+++ components/elements/Button.tsx
@@ -2,7 +2,7 @@ import React, { forwardRef } from "react";
 import clsx from "clsx";
 
 interface IButtonProps {
-  variant?: "default" | "outline" | "plain";
+  variant?: "default" | "white" | "outline" | "plain";
 }
 
 type Ref = HTMLButtonElement;
@@ -27,11 +27,15 @@ const Button = forwardRef<Ref, ButtonProps>((props, ref) => {
       `w-[100px] h-[48px] bg-primary text-white rounded-lg hover:bg-blue-500`,
       className
     ),
-    outline: clsx(
-      `w-[100px] h-[48px] bg-white border border-primary rounded-lg`,
+    white: clsx(
+      "w-[100px] h-[48px] bg-white rounded-lg hover:bg-blue-50",
       className
     ),
-    plain: clsx("w-[100px] h-[48px]", className),
+    outline: clsx(
+      `w-[100px] h-[48px] bg-white border border-primary rounded-lg hover:bg-blue-50`,
+      className
+    ),
+    plain: clsx("w-[100px] h-[48px] rounded-lg hover:bg-blue-50", className),
   };
 
   return (

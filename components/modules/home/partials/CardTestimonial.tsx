0df9923ad6f8b2ef567a637d58e370f14d62f16a
Dandy Candra <dandycandrapratama@gmail.com>
2023-12-21 14:35:07 +0700

    add partials card testimonial

---
+++ components/modules/home/partials/CardTestimonial.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Image from "next/image";
+import { Card } from "@/components/elements";
+import DummyAvatar from "@/public/PNG/dummyAvatar.png";
+
+const CardTestimonial = () => {
+  return (
+    <Card className="w-[544px] p-10 rounded-lg bg-lightGray">
+      <p className="text-xl w-[448px]">
+        &quot;Buying my first home was a dream come true. It was a great feeling
+        to finally have a place that was truly my own, and I could make it into
+        the perfect living space for me and my family.&quot;
+      </p>
+
+      <div className="flex flex-row items-center gap-3 mt-10">
+        <Image src={DummyAvatar} alt="" height={48} />
+
+        <div>
+          <p className="text-sm text-gray">Fujiwara Sugiono</p>
+          <p className="text-sm">Buyer House at Ungaran</p>
+        </div>
+      </div>
+    </Card>
+  );
+};
+
+export default CardTestimonial;

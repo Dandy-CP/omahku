72191303c214df67129c02b0eafef2e681181e2f
Dandy Candra <dandycandrapratama@gmail.com>
2023-12-21 14:32:41 +0700

    add badge and card to index export

--- components/elements/index.ts
+++ components/elements/index.ts
@@ -1,2 +1,4 @@
 export { default as Container } from "./Container";
 export { default as Button } from "./Button";
+export { default as Badge } from "./Badge";
+export { default as Card } from "./Card";

0c4ca4afb9b3170f80359557ab1d6098caf08f7e
Dandy Candra <dandycandrapratama@gmail.com>
2023-12-21 23:02:37 +0700

    add query client and conditional layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,22 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "@/config/queryClient";
 import Layout from "@/components/Layout/Layout";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <QueryClientProvider client={queryClient}>
+      {router.pathname !== "/_error" ? (
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      ) : (
+        <Component {...pageProps} />
+      )}
+    </QueryClientProvider>
   );
 }

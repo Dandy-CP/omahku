dfd1e7f937b11c25eb734fdaf7ddcf15402230ee
Dandy Candra <dandycandrapratama@gmail.com>
2023-12-21 14:35:44 +0700

    add new section to export index

--- components/modules/home/index.ts
+++ components/modules/home/index.ts
@@ -1,2 +1,7 @@
 export { default as HeroSection } from "./HeroSection";
 export { default as SearchSection } from "./SearchSection";
+export { default as TrustCompaniesSection } from "./TrustCompaniesSection";
+export { default as FeatureSection } from "./FeatureSection";
+export { default as FeaturedHouseSection } from "./FeaturedHouseSection";
+export { default as TestimonialSection } from "./TestimonialSection";
+export { default as ContactUsSection } from "./ContactUsSection";
